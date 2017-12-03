844dc616dec76aad3b4047a7618860f2b1605e26
yuanhualiang <jlegend2011@126.com>
2017-12-03 22:05:27 +0800

    初始化

---
+++ project-service/src/main/java/com/green/service/CardAuditPassedRecordService.java
@@ -0,0 +1,16 @@
+package com.green.service;
+
+import com.green.entity.CardAuditPassedRecord;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 卡券审核通过记录表 服务类
+ * </p>
+ *
+ * @author yuanhualiang
+ * @since 2017-12-03
+ */
+public interface CardAuditPassedRecordService extends IService<CardAuditPassedRecord> {
+	
+}
---
+++ project-service/src/main/java/com/green/service/impl/CardAuditPassedRecordServiceImpl.java
@@ -0,0 +1,20 @@
+package com.green.service.impl;
+
+import com.green.entity.CardAuditPassedRecord;
+import com.green.mapper.CardAuditPassedRecordMapper;
+import com.green.service.CardAuditPassedRecordService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 卡券审核通过记录表 服务实现类
+ * </p>
+ *
+ * @author yuanhualiang
+ * @since 2017-12-03
+ */
+@Service
+public class CardAuditPassedRecordServiceImpl extends ServiceImpl<CardAuditPassedRecordMapper, CardAuditPassedRecord> implements CardAuditPassedRecordService {
+	
+}
