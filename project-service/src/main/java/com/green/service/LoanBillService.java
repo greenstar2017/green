94a59c5f0dc90d76416181ca2aa26d24c6f6c0c6
yuanhualiang <jlegend2011@126.com>
2018-03-17 23:29:27 +0800

    小贷项目

---
+++ project-service/src/main/java/com/green/service/LoanBillService.java
@@ -0,0 +1,16 @@
+package com.green.service;
+
+import com.green.entity.LoanBill;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 入款信息 服务类
+ * </p>
+ *
+ * @author yuanhualiang
+ * @since 2018-03-17
+ */
+public interface LoanBillService extends IService<LoanBill> {
+	
+}
---
+++ project-service/src/main/java/com/green/service/impl/LoanBillServiceImpl.java
@@ -0,0 +1,20 @@
+package com.green.service.impl;
+
+import com.green.entity.LoanBill;
+import com.green.mapper.LoanBillMapper;
+import com.green.service.LoanBillService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 入款信息 服务实现类
+ * </p>
+ *
+ * @author yuanhualiang
+ * @since 2018-03-17
+ */
+@Service
+public class LoanBillServiceImpl extends ServiceImpl<LoanBillMapper, LoanBill> implements LoanBillService {
+	
+}
