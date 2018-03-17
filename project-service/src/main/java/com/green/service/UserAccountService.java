34a6ba92002b16280f96cdce1d290f17742ffab1
yuanhualiang <jlegend2011@126.com>
2018-03-17 15:02:18 +0800

    小贷项目

--- project-service/src/main/java/com/green/service/UserAccountService.java
+++ project-service/src/main/java/com/green/service/UserAccountService.java
@@ -5,12 +5,19 @@ import com.baomidou.mybatisplus.service.IService;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author yuanhualiang
  * @since 2018-03-16
  */
 public interface UserAccountService extends IService<UserAccount> {
-	
+
+	/**
+	 * 检查账号是否存在
+	 * 
+	 * @param userAccount
+	 * @return
+	 */
+	boolean checkExistAccount(UserAccount userAccount);
 }
--- project-service/src/main/java/com/green/service/impl/UserAccountServiceImpl.java
+++ project-service/src/main/java/com/green/service/impl/UserAccountServiceImpl.java
@@ -1,9 +1,13 @@
 package com.green.service.impl;
 
+import com.green.constants.AccountStatusEnum;
 import com.green.entity.UserAccount;
 import com.green.mapper.UserAccountMapper;
 import com.green.service.UserAccountService;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +20,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserAccountServiceImpl extends ServiceImpl<UserAccountMapper, UserAccount> implements UserAccountService {
+
+	@Override
+	public boolean checkExistAccount(UserAccount userAccount) {
+		String account = userAccount.getAccount();
+		Wrapper<UserAccount> wrapper = new EntityWrapper<UserAccount>();
+		wrapper.eq("account", account);
+		if(null != userAccount.getId()) {
+			wrapper.ne("id", userAccount.getId());
+		}
+		wrapper.eq("status", AccountStatusEnum.ENABLED.getKey());
+		int count = this.selectCount(wrapper);
+		return count > 0;
+	}
 	
 }
